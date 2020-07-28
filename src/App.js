a83f1b495f62ed13a8d1455c6ac7c111ef0d2e73
Stefano <smfgiordano@gmail.com>
2020-07-28 19:54:50 -0300

    padding top fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 const AppWrapper = styled.div`
   background: var(--grayDark);
-  padding-top: 94px;
+  padding-top: 0px;
 
   @media (max-width: 800px){
     padding-top: 40px;
