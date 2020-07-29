c92098c035d0f3f506efd27be63aaceb20ad7dd3
Stefano <smfgiordano@gmail.com>
2020-07-29 19:13:28 -0300

    Page 404 added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Pagina404 from './pages/Page404/index.js'
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo(){
+  return (
+    <div>
+      Página de Cadastro de Vídeo
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} exact /> 
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route component={Pagina404}/>
+      </Switch>
+    </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+
+
+function Pagina404 (){
+    return(
+        <div className="container">
+        <div className="Description">
+          <div><h1 className="text">Não há nada aqui ¯\_(ツ)_/¯</h1></div>
+        </div>
+        <div className="error404">
+          <div className="image"><img src="http://gif.co/bbhXK.gif" alt="gif" /></div>
+        </div>
+        </div>
+        )
+}
+
+export default Pagina404
