c17e7d1755b0b618364762f905c0a098b2d9bca5
Stefano <smfgiordano@gmail.com>
2020-07-29 22:06:26 -0300

    Improved Page 404

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -8,7 +8,7 @@ function Pagina404 (){
       <PageDefault>
         <div className="container">
         <div className="Description">
-          <div><h1 className="text">Não há nada aquis ¯\_(ツ)_/¯</h1></div>
+          <div><h1 className="text">Não há nada aqui ¯\_(ツ)_/¯</h1></div>
         </div>
         <div className="error404">
           <div className="image"><img src="http://gif.co/bbhXK.gif" alt="gif" /></div>
